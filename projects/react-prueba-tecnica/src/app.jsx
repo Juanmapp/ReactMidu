a7f66dae911ae6116e1650ee3992db76a6681e4d
Juanmapp <juanma1694@gmail.com>
2023-03-25 13:54:02 -0300

    Agregamos estilos e implementamos buenas practicas

--- projects/react-prueba-tecnica/src/app.jsx
+++ projects/react-prueba-tecnica/src/app.jsx
@@ -1,39 +1,46 @@
-// MINUTO 47
+// MINUTO 1.27.30
 
 import { useState, useEffect } from 'react'
+import './app.css'
+import {getRandomFact} from './services/facts.js'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
+    getRandomFact().then(newFact => setFact(fact))
+}, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const firstWord = fact.split(' ', 3).join(" ")
+    console.log(firstWord) 
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?sie_50&color=red&json=true`)  
     .then(res => res.json())
-    .then(data => {
-        const { fact } = data
-        setFact(fact)
-
-        const firstWord = fact.split(' ')[0]
-        console.log(firstWord);
-
-        fetch(`https://cataas.com/cat/says/${firstWord}`)
-        .then(res => res.json())
-        .then(response => {
-            const { url } = response
-            setImageUrl(url)
-        })
+    .then(response => {
+      const { url } = response
+      setImageUrl(url)
     })
-    
-  }, [])
+},  [fact])
 
-
+  const handleClick = async () => {
+  const newFact = await getRandomFact()
+  setFact(newFact)
+}
 
   return (
     <main>
+      <button onClick={handleClick}>Get new fact</button>
       <h1>App de gatitos</h1>
-      { fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image from ${fact}`} />}
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image from ${fact}`} />}
     </main>
   )
 }
