63eed64a1bb40d145f3f653348145a277152b3e3
Juanmapp <juanma1694@gmail.com>
2023-03-28 14:34:35 -0300

    Agregando el buscador

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,21 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import responseMovies from './mocks/withResults.json'
+import noResults from './mocks/noResults.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const movies = responseMovies.Search
+    const hasMovies = movies?.length > 0
 
   return (
     
-    <div>
-      <form className='form'>
-
-        <input placeholder='Star Wars, The Matrix, Avengers...'/>
-        <button>Buscar</button>
-
+    <div className='page'>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className='form'>
+          <input placeholder='Star Wars, The Matrix, Avengers...'/>
+          <button type="submit">Buscar</button>
       </form>
-    
+      </header>
+
+      <main>
+        {
+          hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title}/>
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          : 
+          <p>
+            No se encontraron peliculas para esta busqueda
+          </p>
+        }
+
+      </main>
     </div>
   
   )
