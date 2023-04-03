fcbbb9e3d032b38ec2f57057be28cd37752361fa
Juanmapp <juanma1694@gmail.com>
2023-04-03 17:46:56 -0300

    Agregando filtros de precio y categoria

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,12 +1,35 @@
+// MINUTO 1:00:00
+import {products as initicalProducts} from './mocks/products.json'
 import { Products } from "./components/Products.jsx"
-import {products} from './mocks/products.json'
+import { Header } from "./components/Header.jsx"
+
+import { useState } from 'react'
+
 
 function App() {
-  
+  const [products] = useState(initicalProducts)
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice : 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        filters.category === "all" ||
+        filters.category === product.category
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
-
-    <Products products={products}/>
+    <>
+    <Header changeFilters={setFilters} />
+    <Products products={filteredProducts}/>
+    </>
   
   )
 }
---
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,46 @@
+import './filters.css'
+import {useState, useId} from 'react'
+
+export function Filters( {onChange} ) {
+    const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice: event.target.value
+        }))
+    }
+
+    const handleChangeCategory = (event) => {
+        onChange(prevState => ({
+            ...prevState,
+            category: event.target.value
+        }))
+    }
+    return (
+        <section className="filters">
+        <div>
+            <label htmlFor={minPriceFilterId}>Precio a partir de: </label>
+            <input
+                type="range"
+                id={minPriceFilterId}
+                min="0"
+                max="1000"
+                onChange={handleChangeMinPrice}
+            />
+            <span>${minPrice}</span>
+        </div>
+        <div>
+            <label htmlFor={categoryFilterId}>Categoria</label>
+            <select id={categoryFilterId} onChange={handleChangeCategory}>
+                <option value='all'>Todas</option>
+                <option value='laptops'>Portatiles</option>
+                <option value='smartphones'>Celulares</option>
+            </select>
+        </div>
+        </section>
+    )
+}
